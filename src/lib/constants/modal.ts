21a1c48515652b9d91c5505c60803df9b10f72d7
Daniel McCauley <webapp@calvarytucson.com>
2026-05-27 12:21:28 -0700

    feat: add modal animation constants

---
+++ src/lib/constants/modal.ts
@@ -0,0 +1,6 @@
+export const EXIT_DURATION = 150
+
+export const MODAL_ENTER_ANIMATION =
+  'motion-safe:animate-[modal-slide-up_0.2s_cubic-bezier(0.32,0.72,0,1)]'
+export const MODAL_EXIT_ANIMATION =
+  'motion-safe:animate-[modal-slide-down_0.15s_ease-in_forwards]'
